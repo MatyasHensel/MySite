6c07c1c2500b81b3f19934934d2cc9c57a613bcb
MatyasHensel <matyas.hensel2004@gmail.com>
2025-07-28 11:42:16 +0200

    small fix

--- mySiteBackend.js
+++ mySiteBackend.js
@@ -18,22 +18,18 @@ https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
 });
 
 
-
+/*
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.ip} requested ${req.method} ${req.url} Host: ${req.headers.host}`);
   next();
 });
+*/
 
 app.use((req, res, next) => {
   if (req.path.startsWith('/.')) {
     return res.status(403).send('Access denied');
   }
   next();
-});
-
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.url} from ${req.ip}`);
-  next(); // pass control to the next middleware/handler
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
